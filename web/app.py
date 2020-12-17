f8fefc0de57430bcff251c3ff59288fe9a72e157
janpresperin <prespjan@fel.cvut.cz>
2020-12-17 19:57:42 +0100

    final  swagger

--- web/app.py
+++ web/app.py
@@ -6,8 +6,21 @@ from utils.config import set_app_config
 from utils.wikirequests import car_summary_from_wiki
 from utils.youtuberequests import query_yt
 import uuid
+
+from flask_swagger_ui import get_swaggerui_blueprint
 app = Flask(__name__)
 set_app_config(app)
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "CarClassifier"
+    }
+)
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
@@ -19,14 +32,16 @@ def classify():
         file = request.files['file']
         hash_with_filename = str(uuid.uuid4().hex)+"_"+file.filename
         filename = secure_filename(hash_with_filename)
-
         filepath = os.path.join(app.config["IMAGE_UPLOADS"], filename)
         file.save(filepath)
         classificationresult = get_car_probabilities(filepath, app.config["MODEL_PATH"], app.config["CLASSMAPPING_PATH"])
         session["classification_result"] = classificationresult
         session["filename"] = hash_with_filename
     else:
-        classificationresult = session["classification_result"]
+        try:
+            classificationresult = session["classification_result"]
+        except:
+            return url_for("/")
     return render_template('prediction.html', predictions=classificationresult)
 
 
@@ -41,10 +56,18 @@ def get_car_details(car):
         print(wiki_summary)
         session["wiki_summary"] = wiki_summary
         session["reviews"] = car_reviews
+
     else:
-        car_reviews = session["reviews"]
-        wiki_summary = session["wiki_summary"]
-    return render_template('cardetails.html', wiki=wiki_summary,reviews=car_reviews, carfilename = session["filename"], car = car)
+        try:
+            car_reviews = session["reviews"]
+            wiki_summary = session["wiki_summary"]
+        except:
+            return url_for("/")
+    try:
+        car_file = session["filename"]
+    except:
+        car_file = "No file was found"
+    return render_template('cardetails.html', wiki=wiki_summary,reviews=car_reviews, carfilename = car_file, car = car)
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
--- web/utils/config.py
+++ web/utils/config.py
@@ -1,7 +1,10 @@
 import os
 
 def set_app_config(app):
-    app.config["PROJECT_PATH"] = os.getenv("PROJECT_PATH")
+
+    #app.config["PROJECT_PATH"] = os.getenv("PROJECT_PATH")
+    app.config["PROJECT_PATH"] = "/Users/janpresperin/Desktop/VIA/CarClassifierFlask/web"
+
     app.config["IMAGE_UPLOADS"] = os.path.join(app.config["PROJECT_PATH"], "uploads")
     app.config["MODEL_PATH"] = os.path.join(app.config["PROJECT_PATH"], "model/cars196model.h5")
     app.config["CLASSMAPPING_PATH"] = os.path.join(app.config["PROJECT_PATH"], "model/class_mappings.csv")
