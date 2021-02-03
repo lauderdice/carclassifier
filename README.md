99d4bc1d2d9efff4c08736a6148b8c7473df2ab8
janpresperin <prespjan@fel.cvut.cz>
2021-02-03 12:54:51 +0100

    edited readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 1. Run classifier.py file in classifier folder. This fill generate a model file (I assume some basic knowledge of Tensorflow)
-2. Change app.config["MODEL_PATH"] in utils/config to accomodate where your model is located.
+2. Change app.config["MODEL_PATH"] in web/utils/config to accomodate where your model is located. Also make sure to change other configs.
 3. Build with:
 `docker-compose up --build`
--- web/utils/config.py
+++ web/utils/config.py
@@ -3,11 +3,11 @@ import os
 def set_app_config(app):
 
     #app.config["PROJECT_PATH"] = os.getenv("PROJECT_PATH")
-    app.config["PROJECT_PATH"] = "/Users/janpresperin/Desktop/VIA/CarClassifierFlask/web"
+    #app.config["PROJECT_PATH"] = "/Users/janpresperin/Desktop/VIA/CarClassifierFlask/web"
 
-    app.config["IMAGE_UPLOADS"] = os.path.join(app.config["PROJECT_PATH"], "uploads")
-    app.config["MODEL_PATH"] = os.path.join(app.config["PROJECT_PATH"], "model/cars196model.h5")
-    app.config["CLASSMAPPING_PATH"] = os.path.join(app.config["PROJECT_PATH"], "model/class_mappings.csv")
+    #app.config["IMAGE_UPLOADS"] = os.path.join(app.config["PROJECT_PATH"], "uploads")
+    #app.config["MODEL_PATH"] = os.path.join(app.config["PROJECT_PATH"], "model/cars196model.h5")
+    #app.config["CLASSMAPPING_PATH"] = os.path.join(app.config["PROJECT_PATH"], "model/class_mappings.csv")
     app.config["PROJECT_PATH"] = "/Users/janpresperin/Desktop/VIA/CarClassifierFlask/web"
     app.config["IMAGE_UPLOADS"] =  "./uploads"
     app.config["MODEL_PATH"] =  "./model/cars196model.h5"
